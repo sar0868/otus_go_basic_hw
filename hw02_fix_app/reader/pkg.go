6c69a38bbcab35799e08e8362c473639e971c3e4
sar0868 <safarov-ar@yandex.ru>
2024-10-18 11:47:30 +0300

    hw02: resolved comment

--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -13,6 +13,7 @@ func ReadJSON(filePath string) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
+		return nil, err
 	}
 
 	bytes, err := io.ReadAll(f)
