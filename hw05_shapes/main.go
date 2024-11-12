b55d4fd41fc0fa34a24e0eb998b8475a781eabdc
sar0868 <safarov-ar@yandex.ru>
2024-11-12 19:55:42 +0300

    hw05: calculateArea(s any)

--- hw05_shapes/main.go
+++ hw05_shapes/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/sar0868/sar0868/otus_go_basic_hw/hw05_shapes/models/circle"
@@ -9,8 +10,12 @@ import (
 	"github.com/sar0868/sar0868/otus_go_basic_hw/hw05_shapes/shape"
 )
 
-func calculateArea(s shape.Shape) float64 {
-	return s.Area()
+func calculateArea(s any) (float64, error) {
+	if shape, ok := s.(shape.Shape); ok {
+		return shape.Area(), nil
+	}
+	err := errors.New("don't Shape")
+	return 0, err
 }
 
 type Square struct {
@@ -28,10 +33,25 @@ func main() {
 	rectangle := rectangle.NewRectangle(10, 5)
 	triangle := triangle.NewTriangle(8, 6)
 	square := NewSquare(20)
-	fmt.Printf("square side = %d\n", square.Side)
 
-	fmt.Println(calculateArea(circle))
-	fmt.Println(calculateArea(rectangle))
-	fmt.Println(calculateArea(triangle))
-	// fmt.Println(calculateArea(square))
+	areaCircle, err := calculateArea(circle)
+	if err != nil {
+		fmt.Errorf("circle calculateArea: %w", err)
+	}
+	fmt.Println(areaCircle)
+	areaRect, err := calculateArea(rectangle)
+	if err != nil {
+		fmt.Errorf("rectangle calculateArea: %w", err)
+	}
+	fmt.Println(areaRect)
+	areaTriangle, err := calculateArea(triangle)
+	if err != nil {
+		fmt.Errorf("triangle calculateArea: %w", err)
+	}
+	fmt.Println(areaTriangle)
+	areaSquare, err := calculateArea(square)
+	if err != nil {
+		fmt.Errorf("square calculateArea: %w", err)
+	}
+	fmt.Println(areaSquare) 
 }
