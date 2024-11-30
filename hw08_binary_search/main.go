badabb6c9d6186eb17ffacd43e8d2eb905b7292c
sar0868 <safarov-ar@yandex.ru>
2024-11-30 19:42:57 +0300

    hw08 done for int

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	arr := []int{1, 2, 3, 4}
+	ind, res := BinarySearch(arr, 3)
+	fmt.Println(ind, res)
+}
+
+func BinarySearch(data []int, el int) (int, bool) {
+	k := len(data) - 1
+	p := 0
+	for p < k {
+		ind := p + (k-p)/2
+		if data[ind] == el {
+			return ind, true
+		}
+		if el < data[ind] {
+			k = ind
+		} else {
+			p = ind
+		}
+	}
+	return -1, false
 }
---
+++ hw08_binary_search/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_BinarySearch(t *testing.T) {
+	tests := []struct {
+		name   string
+		array  []int
+		find   int
+		want   int
+		result bool
+	}{
+		{
+			name:   "Search 3 from array [1 , 2, 3, 4] expected 2, true",
+			array:  []int{1, 2, 3, 4},
+			find:   3,
+			want:   2,
+			result: true,
+		},
+		{
+			name:   "Search 1 from array [2 , 3, 4] expected  -1, false",
+			array:  []int{2, 3, 4},
+			find:   1,
+			want:   -1,
+			result: false,
+		},
+		{
+			name:   "Search 8 from array [2, 3, 4, 5, 8, 9, 12, 15, 18] expected 4, true",
+			array:  []int{2, 3, 4, 5, 8, 9, 12, 15, 18},
+			find:   8,
+			want:   4,
+			result: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, res := BinarySearch(tt.array, tt.find)
+			assert.Equal(t, got, tt.want)
+			assert.Equal(t, res, tt.result)
+		})
+	}
+}
