f4a2bcfcadb4010dddd31c69aef2799e286ee5f2
sar0868 <safarov-ar@yandex.ru>
2024-12-05 12:49:38 +0300

    hw09 in progress

--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Book struct {
+	ID     int     `json:"id"`
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Year   int     `json:"year"`
+	Size   int     `json:"size"`
+	Rate   float64 `json:"rate"`
+	Sample []byte  `json:"sample"`
+}
+
+func WriteJSON(books []Book) ([][]byte, error) {
+	// book := Book{
+	// 	ID:     1,
+	// 	Title:  "title",
+	// 	Author: "author",
+	// 	Year:   2024,
+	// 	Size:   10,
+	// 	Rate:   1,
+	// 	Sample: []byte{1, 2, 3},
+	// }
+	result := make([][]byte, 0)
+	for _, v := range books {
+		book := v
+		j, err := json.Marshal(&book)
+		if err != nil {
+			fmt.Printf("Error: %v", err)
+			return nil, err
+		}
+		fmt.Printf("%s\n", j)
+		result = append(result, j)
+	}
+
+	return result, nil
+}
+
 func main() {
-	// Place your code here.
+	book := Book{
+		ID:     1,
+		Title:  "title",
+		Author: "writer",
+		Year:   2024,
+		Size:   10,
+		Rate:   1,
+		Sample: []byte{1, 2, 3},
+	}
+	books := []Book{book, book, book}
+	j, err := WriteJSON(books)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s\n", j)
 }
---
+++ hw09_serialize/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWriteJSON(t *testing.T) {
+	type args struct {
+		books []Book
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name:    "Write slice json without errors",
+			args:    args{[]Book{{ID: 1, Title: "title", Author: "writer", Year: 2024, Size: 10, Rate: 1.1, Sample: []byte{1}}}},
+			want:    `{"id":1,"title":"title","author":"writer","year":2024,"size":10,"rate":1.1,"sample":"A"}`,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := WriteJSON(tt.args.books)
+			j := fmt.Sprintf("%v", got)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("WriteJSON() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			assert.Equal(t, j, tt.want)
+			// if !reflect.DeepEqual(j, tt.want) {
+			// 	t.Errorf("WriteJSON() = %v, want %v", got, tt.want)
+			// }
+		})
+	}
+}
