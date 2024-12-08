5b5f2528b6b87dec140c9735eeeb1c889cf03ce6
sar0868 <safarov-ar@yandex.ru>
2024-12-08 17:39:41 +0300

    hw09: xml, yaml - done, gob done

--- hw09_serialize/book_yml/pkg_test.go
+++ hw09_serialize/book_yml/pkg_test.go
@@ -1,4 +1,4 @@
-package bookxml
+package bookyml
 
 import (
 	"testing"
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWriteReadXML(t *testing.T) {
+func TestWriteReadYML(t *testing.T) {
 	type args struct {
 		books []Book
 	}
@@ -16,32 +16,32 @@ func TestWriteReadXML(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "Write and read slice xml without errors",
+			name:    "Write and read slice yml without errors",
 			args:    args{[]Book{{1, "title", "writer", 2024, 10, 1.1, []byte("A")}}},
 			wantErr: false,
 		},
 		{
-			name:    "Write and read slice xml without errors, Book empty",
+			name:    "Write and read slice yml without errors, Book empty",
 			args:    args{[]Book{}},
 			wantErr: false,
 		},
 		{
-			name:    "Write and read slice xml without errors, 2 Book",
+			name:    "Write and read slice yml without errors, 2 Book",
 			args:    args{[]Book{{Sample: []byte("")}, {Sample: []byte("")}}},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := WriteXML(tt.args.books)
+			got, err := WriteYML(tt.args.books)
 			// j := fmt.Sprintf("%s", got)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("WriteXML() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("WriteYML() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 
 			// assert.Equal(t, j, tt.want)
-			result := ReadXML(got)
+			result := ReadYML(got)
 			assert.Equal(t, result, tt.args.books)
 		})
 	}
