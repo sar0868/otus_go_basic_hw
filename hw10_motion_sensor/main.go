f9ba9d6c249c2ff3a6e404471895f823a731f4c6
sar0868 <safarov-ar@yandex.ru>
2024-12-15 21:53:15 +0300

    in progress

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -6,18 +6,33 @@ import (
 	"time"
 )
 
+var CLOSEA bool
+
 func Sensor(data chan<- float64) {
+	cnt := 0
 	for {
+		if CLOSEA{
+			return
+		}
 		data <- rand.Float64() * 100
-		time.Sleep(250 * time.Millisecond)
+		time.Sleep(100 * time.Millisecond)	
+		cnt++	
+		if cnt == 20{
+			close(data)
+
+			CLOSEA = true
+		}
 	}
+
 }
 
 func GetData(data <-chan float64, processedData chan<- float64) {
 	var sum float64
 	cnt := 0
-	for {
-		value := <-data
+	for value := range data{
+		// if CLOSEA {
+		// 	break
+		// }
 		sum += value
 		cnt++
 		if cnt == 10 {
@@ -26,21 +41,45 @@ func GetData(data <-chan float64, processedData chan<- float64) {
 			cnt = 0
 		}
 	}
+	close(processedData)
+
+	// for {
+	// 	if CLOSEA {
+	// 		close(processedData)
+	// 		return
+	// 	}
+	// 	value := <-data
+	// 	sum += value
+	// 	cnt++
+	// 	if cnt == 10 {
+	// 		processedData <- sum / float64(cnt)
+	// 		sum = 0
+	// 		cnt = 0
+	// 	}
+	// }
 }
 
 func Info(processedData <-chan float64) {
-	for {
-		fmt.Println(<-processedData)
+	for mean := range processedData{
+		fmt.Println(mean)
 	}
+	// for {
+	// 	fmt.Println(<-processedData)
+	// }
 }
 
 func main() {
 	data := make(chan float64)
 	processedData := make(chan float64)
+	
 
 	go Info(processedData)
 	go GetData(data, processedData)
 	go Sensor(data)
+	select{
+	
+	}
 
-	time.Sleep(1 * time.Minute)
+
+	// time.Sleep(1 * time.Minute)
 }
