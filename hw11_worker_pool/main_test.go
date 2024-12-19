9ca035c9a02b038c2356b251274525d01c515012
sar0868 <safarov-ar@yandex.ru>
2024-12-19 13:18:19 +0300

    hw10: edit test

--- hw11_worker_pool/main_test.go
+++ hw11_worker_pool/main_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var mx sync.Mutex
+
 func TestCounter(t *testing.T) {
 	tests := []struct {
 		name            string
@@ -26,7 +28,7 @@ func TestCounter(t *testing.T) {
 	}
 	for _, tt := range tests {
 		var wg sync.WaitGroup
-		var mx sync.Mutex
+
 		cnt = 0
 		for i := 0; i < tt.countGoroutines; i++ {
 			wg.Add(1)
