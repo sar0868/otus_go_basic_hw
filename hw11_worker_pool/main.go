8d6d6942cc79299541caba293a99e205b6ba0248
sar0868 <safarov-ar@yandex.ru>
2024-12-19 22:06:26 +0300

    resolved comment

--- hw11_worker_pool/main.go
+++ hw11_worker_pool/main.go
@@ -20,13 +20,22 @@ func Counter(n int, wg *sync.WaitGroup, mx *sync.Mutex, ch chan string) {
 	ch <- fmt.Sprintf("Counter %d completed work, cnt=%d done", n, cnt)
 }
 
+func Info(ch chan string, wg *sync.WaitGroup){
+	defer wg.Done()
+	fmt.Println(<-ch)
+}
+
 func main() {
 	var wg sync.WaitGroup
 	ch := make(chan string)
+
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go Counter(i, &wg, &mx, ch)
-		fmt.Println(<-ch)
+		wg.Add(1)
+		go Info(ch, &wg)
 	}
+
+
 	wg.Wait()
 }
