f586c0b24fa25f7103b7767acd0f5ecdd7dc6506
sar0868 <safarov-ar@yandex.ru>
2024-12-19 19:15:56 +0300

    edit test

--- hw11_worker_pool/main_test.go
+++ hw11_worker_pool/main_test.go
@@ -13,11 +13,11 @@ func TestCounter(t *testing.T) {
 		countGoroutines int
 		want            int
 	}{
-		// {
-		// 	name:            "Test 1 gorutina, count=3",
-		// 	countGoroutines: 1,
-		// 	want:            3,
-		// },
+		{
+			name:            "Test 1 gorutina, count=3",
+			countGoroutines: 1,
+			want:            3,
+		},
 		{
 			name:            "Test 3 gorutins, count=9",
 			countGoroutines: 3,
@@ -26,7 +26,6 @@ func TestCounter(t *testing.T) {
 	}
 	for _, tt := range tests {
 		var wg sync.WaitGroup
-		// var mx sync.Mutex
 		cnt = 0
 		for i := 0; i < tt.countGoroutines; i++ {
 			wg.Add(1)
