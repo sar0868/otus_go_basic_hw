aa31f622c7c9cc985fc93dc45ee54693df77fe2c
sar0868 <safarov-ar@yandex.ru>
2025-01-10 13:27:49 +0300

    add .env and example.log files

--- hw12_log_util/go.mod
+++ hw12_log_util/go.mod
@@ -1,3 +1,5 @@
 module github.com/sar0868/otus_go_basic_hw/hw12_log_util
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
--- hw12_log_util/main.go
+++ hw12_log_util/main.go
@@ -1,9 +1,30 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 // 2025-01-09 12:00:00 [TRACE, DEBUG, INFO, WARN, ERROR, FATAL] [module=1...5] text
 // статистика модуль уровень=(%от всех сообщений по модулю count/sum(count)),
 // уровень=(% от всех записей c данным уровнем в файле [count/sum(level)])
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	// fmt.Println(os.Getenv("LOG_ANALYZER_FILE"))
+	var file string
+	var level string
+	var output string
+	flag.StringVar(&file, "file", os.Getenv("LOG_ANALYZER_FILE"), "path log file")
+	flag.StringVar(&level, "level", os.Getenv("LOG_ANALYZER_LEVEL"), "level for analysis")
+	flag.StringVar(&output, "output", os.Getenv("LOG_ANALYZER_OUTPUT"), "path for output file")
+	flag.Parse()
+	fmt.Println(file, level, output)
 	// Place your code here.
 }
