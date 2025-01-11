e025f7d8276bd26084cdf10d538cd7b815c8e81c
sar0868 <safarov-ar@yandex.ru>
2025-01-11 14:55:15 +0300

    add test Statistic

--- hw12_log_util/main.go
+++ hw12_log_util/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -12,6 +13,12 @@ import (
 // 2025-01-09 12:00:00 [TRACE, DEBUG, INFO, WARN, ERROR, FATAL] [module=1...5] text
 // статистика модуль уровень=(%от всех сообщений по модулю count/sum(count)),
 // уровень=(% от всех записей c данным уровнем в файле [count/sum(level)])
+
+type Statistic struct {
+	level   string
+	modules map[string]int
+	all     int
+}
 
 func main() {
 	if err := godotenv.Load(); err != nil {
@@ -32,9 +39,24 @@ func ReadFile(path string) error {
 	return nil
 }
 
-func CalcStatistics(data []string) map[string]int {
-	fmt.Println(data)
-	return make(map[string]int)
+func CalcStatistics(data []string, level string) Statistic {
+	modulesLevel := map[string]int{}
+	modulesLevelSum := map[string]int{}
+	for _, el := range data {
+		arr := strings.Split(el, " ")
+		modulesLevelSum[arr[3]]++
+		if arr[2] == level {
+			modulesLevel[arr[3]]++
+		}
+	}
+	modules := map[string]int{}
+	sumLevel := 0
+
+	for module, item := range modulesLevel {
+		modules[module] = 100 * item / modulesLevelSum[module]
+		sumLevel += modulesLevel[module]
+	}
+	return Statistic{level: level, modules: modules, all: 100 * sumLevel / len(data)}
 }
 
 func WriteFile(data map[string]int) error {
