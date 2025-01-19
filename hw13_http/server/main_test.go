1169434237a97c65f8f34269ab08576442890bdf
sar0868 <safarov-ar@yandex.ru>
2025-01-19 23:18:58 +0300

    hw13 in progress

--- hw13_http/server/main_test.go
+++ hw13_http/server/main_test.go
@@ -1,41 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "Hello, client")
-// }
+func TestHandler(t *testing.T) {
+	req := httptest.NewRequest("GET", "/users", nil)
+	w := httptest.NewRecorder()
+	data := string("[{\"id\":1,\"name\":\"Aleksey\",\"age\":56,\"address\":\"Tver\"}," +
+		"{\"id\":2,\"name\":\"Irina\",\"age\":60,\"address\":\"Tver\"}," +
+		"{\"id\":3,\"name\":\"Maria\",\"age\":27,\"address\":\"Tver\"}]\n")
 
-// func TestHandler(t *testing.T) {
-// req := httptest.NewRequest("GET", "/users", nil)
-// w := httptest.NewRecorder()
-// getUsers(w, req)
+	getUsers(w, req)
 
-// res := w.Result()
-// if res.StatusCode != http.StatusOK {
-// 	t.Errorf("expected status 200, got %v", res.StatusCode)
-// }
-// }
+	res := w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK, got %v", res.StatusCode)
+	}
+	body := w.Body.String()
 
-func Test_getUsers(t *testing.T) {
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			getUsers(tt.args.w, tt.args.r)
-			fmt.Print(t)
-		})
-	}
+	assert.Equal(t, data, body)
 }
